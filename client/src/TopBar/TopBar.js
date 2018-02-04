5d367b35c7538b1b5b2710bc3f46b3c0369b17b7
henryhuang <h1886@outlook.com>
2018-02-04 19:24:46 +0800

    add semantic ui to client

---
+++ client/src/TopBar/TopBar.js
@@ -0,0 +1,48 @@
+/**
+ * Created by Henry Huang.
+ */
+import React, { Component } from 'react';
+import { Menu } from 'semantic-ui-react';
+import Logo from '../res/images/tb6-logo.svg';
+
+export default class MenuExampleStackable extends Component {
+  state = {};
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+    const { activeItem } = this.state;
+
+    return (
+      <Menu stackable>
+        <Menu.Item>
+          <img src={Logo}/>
+        </Menu.Item>
+
+        <Menu.Item
+          name='features'
+          active={activeItem === 'features'}
+          onClick={this.handleItemClick}
+        >
+          Features
+        </Menu.Item>
+
+        <Menu.Item
+          name='testimonials'
+          active={activeItem === 'testimonials'}
+          onClick={this.handleItemClick}
+        >
+          Testimonials
+        </Menu.Item>
+
+        <Menu.Item
+          name='sign-in'
+          active={activeItem === 'sign-in'}
+          onClick={this.handleItemClick}
+        >
+          Sign-in
+        </Menu.Item>
+      </Menu>
+    )
+  }
+}
