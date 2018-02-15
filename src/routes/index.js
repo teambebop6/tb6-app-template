ff37e65defcfd4cfc38271df7b91cc873b5c5a14
henryhuang <h1886@outlook.com>
2018-02-15 11:30:50 +0800

    server: auth route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
+import auth from './auth';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.json({
-    message: 'Welcome to TB6.'
+    result: 'Welcome to TB6.'
   });
 });
+
+router.use('/auth', auth);
 
 module.exports = router;
