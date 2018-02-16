206b094cef03028bc81dff768a8419ce2886471a
Florian Rudin <flaudre@gmail.com>
2018-02-16 15:00:13 +0800

    added admin subrouter

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,15 @@
 import express from 'express';
 
+// Load subroutes
+import admin_router from './admin'
+
 const router = express.Router();
+
+
+
+
+// Initialize subroutes
+router.use('/admin', admin_router)
 
 router.get('/', (req, res) => {
   res.json({
