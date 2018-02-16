1f1ee7a5cf0c194681215f6d503ff034979fe670
Florian Rudin <flaudre@gmail.com>
2018-02-16 21:22:44 +0800

    Plugged in subroute of crud template

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,12 @@ import express from 'express';
 import auth from './auth';
 
 // Load subroutes
-import admin_router from './admin'
+import admin_crud_router from './admin/crudTemplate'
 
 const router = express.Router();
 
-
-
-
 // Initialize subroutes
-router.use('/admin', admin_router)
+router.use('/admin/crudTemplate', admin_crud_router)
 
 router.get('/', (req, res) => {
   res.json({
