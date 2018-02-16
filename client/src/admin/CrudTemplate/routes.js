047fe71cbd83f905c9b9f85004a2b5502927b034
Florian Rudin <flaudre@gmail.com>
2018-02-16 14:38:14 +0800

    Added custom router

--- client/src/admin/CrudTemplate/routes.js
+++ client/src/admin/CrudTemplate/routes.js
@@ -1,34 +1,20 @@
 import React from 'react';
-import { Route, Link} from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import TopBar from '../common/TopBar/TopBar';
-const path = require('path');
 
+// Subroutes
+import List from './List';
+import Add from './Add';
+
+const path = require('path');
 const router = ({ match }) => (
   <div>  
     <TopBar/>
-  
-    <ul>
-      <li>
-        <Link to={path.join(match.url, './')}>List items</Link>
-      </li>
-      <li>
-        <Link to={path.join(match.url, './add')}>Add item</Link>
-      </li>
-    </ul>
 
-    <Route path={path.join(match.url, './add')} component={AddItem} />
+    <Route path={path.join(match.url, './add')} component={Add} />
+    <Route exact path={path.join(match.url, '/')} component={List} />
 
-    <Route exact path={path.join(match.url, '/')} render={() =>
-      <h3>Items list</h3>
-    }
-    />
   </div>
 )
-
-const AddItem = () => (
-  <div>
-    <h3>Add item</h3>
-  </div>
-);
 
 export default router;
