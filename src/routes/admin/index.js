f7a6208dd4d2837088a49509af32ad6c7854b702
Florian Rudin <flaudre@gmail.com>
2018-02-16 14:59:55 +0800

    added multipart handler

--- src/routes/admin/index.js
+++ src/routes/admin/index.js
@@ -1,12 +1,38 @@
 /***
- * Basic CRUD for site admin
+ *
+ *   Generic CRUD template for item management
+ * 
  * **/
+
 
 import express from 'express';
 
+var multer = require('multer');
+var path = require('path');
+var mkdirp = require('mkdirp');
+var crypto = require('crypto');
+var mime = require('mime');
+
 const router = express.Router();
 
+// Load relevant model
 const Model = require('../../models/testModel');
+
+// Configure multipart handler & upload destination
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    var p = path.join(req.app.locals.config.UPLOAD_FOLDER, '/example/');
+    mkdirp.sync(p);
+    cb(null, p)
+  },
+  filename: function (req, file, cb) {
+    crypto.pseudoRandomBytes(16, function (err, raw) {
+      cb(null, raw.toString('hex') + Date.now() + '.' + mime.getExtension(file.mimetype));
+    });
+  }
+});
+var upload = multer({storage: storage});
+
 
 // List
 router.get('/', (req, res, next) => {
@@ -16,9 +42,21 @@ router.get('/', (req, res, next) => {
   });
 });
 
+
 // Create
-router.post('/add', (req, res) => {
-  var item = new Model(req.body.item);
+router.post('/add', upload.fields(
+[
+  { name: 'avatar', maxCount: 1 },
+]
+), (req, res, next) => {
+  console.log(req.body);
+  var item = new Model(req.body);
+  console.log("Adding new item:");
+  console.log(item);
+
+  // Files
+  item.avatar = req.files.avatar;
+  // ...
 
   item.save((err) => {
     if(err){ res.json(err); }
